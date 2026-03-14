14ae0055a99226d8bd46dc1415bfc56257f4e8ef
annazlapinilihayu-ship-it <sonzaixai@gmail.com>
2026-03-14 20:20:00 +0700

    clean

--- run.js
+++
@@ -1,8 +0,0 @@
-console.log('🚀=== Panel Siap Digunakan ===🚀');
-console.log('📂 Silahkan unggah file bot mu di tab File 📂');
-console.log('🌟 Images Docker by Sonzai X シ');
-console.log('📱 Telegram: November2k');
-console.log('💻 GitHub: github.com/SonzaiEkkusu');
-require('child_process').spawn('bash', [], {
-  stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-});
